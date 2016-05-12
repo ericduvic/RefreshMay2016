0c5adf8168672bd4d8b49c6470aa568cfc5f7788
Eric Duvic <ericduvic@gmail.com>
2016-05-12 18:23:18 -0500

    Integrate basic Postgres db functionality

--- server/coolest_chat.rb
+++ server/coolest_chat.rb
@@ -1,20 +1,34 @@
 require 'sinatra'
 require 'oj'
+require 'pg'
 
-message_queue = [{
-    'type' => 'message',
-    'id' => 1,
-    'attributes' => {
-      'timestamp' => '20160512 12:30:00',
-      'author' => 'Eric Duvic',
-      'message' => 'Hello refreshers!'
-  }}]
+connection = PG.connect(
+  host: 'localhost',
+  dbname: 'eric',
+  user: 'eric',
+  password: ''
+)
 
 get '/' do
   "Hello world!"
 end
 
 get '/messages' do
+  message_queue = []
+  connection.exec("SELECT * FROM messages") do |result|
+    result.each do |row|
+      message_queue << {
+        'type' => 'message',
+        'id' => row['id'],
+        'attributes' => {
+          'system_create_time' => row['system_create_time'],
+          'author' => row['author'],
+          'message' => row['message']
+        }
+      }
+    end
+  end
+
   content_type "application/vnd.api+json"
   body Oj.dump({
     'data' => message_queue
@@ -22,14 +36,25 @@ get '/messages' do
 end
 
 post '/messages' do
-  data = Oj.load(request.body)['data']
-  new_id = message_queue.length + 1
-  data['id'] = new_id
-  message_queue << data
+  data = nil
+  attributes = Oj.load(request.body)['data']['attributes']
+  connection.exec_params("INSERT INTO messages (author, message) VALUES ($1, $2) RETURNING *", [attributes['author'], attributes['message']]) do |result|
+    result.each do |row|
+      data = {
+        'type' => 'message',
+        'id' => row['id'],
+        'attributes' => {
+          'system_create_time' => row['system_create_time'],
+          'author' => row['author'],
+          'message' => row['message']
+        }
+      }
+    end
+  end
 
   content_type "application/vnd.api+json"
   status 201
-  headers "Location" => "#{request.base_url}/messages/#{new_id}"
+  headers "Location" => "#{request.base_url}/messages/#{data['id']}"
   body Oj.dump({
     'data' => data
     })
