7885174f077e688673f53228db1eba7213180c11
Eric Duvic <ericduvic@gmail.com>
2016-05-24 22:02:03 -0500

    Updated default.pp to better handle postgres setup

--- manifests/default.pp
+++ manifests/default.pp
@@ -53,15 +53,24 @@ node 'web' {
 }
 
 node 'db' {
+  class { 'postgresql::globals':
+    manage_package_repo => true,
+    version => '9.5',
+  }
+
   class { 'postgresql::server':
     listen_addresses => '*',
   }
 
-  postgresql::server::db { 'coolest_chat':
-    user => 'chatapp',
-    password => postgresql_password('chatapp', 'V3ry$3cR37')
+  postgresql::server::role { 'chatapp':
+    password_hash => postgresql_password('chatapp', 'V3ry$3cR37'),
   }
 
+  postgresql::server::database { 'coolest_chat':
+    owner => 'chatapp',
+    require => Postgresql::Server::Role['chatapp'],
+  }
+  
   postgresql::server::pg_hba_rule { 'Allow access from web host':
     description => 'Open up access from 192.168.33.0/24',
     type => 'host',
@@ -69,5 +78,13 @@ node 'db' {
     user => 'chatapp',
     address => '192.168.33.0/24',
     auth_method => 'md5',
+  }
+
+  exec { 'Create database tables':
+    command => 'psql -v ON_ERROR_STOP=1 -d coolest_chat -t -f /vagrant/database/db_script.sql',
+    path => '/usr/bin',
+    require => Postgresql::Server::Database['coolest_chat'],
+    user => 'postgres',
+    group => 'postgres',
   }
 }
